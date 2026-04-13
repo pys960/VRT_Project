3056a103ee598e65ae694a4bf023afbb4106bd26
박영석 <93813135+pys960@users.noreply.github.com>
2026-04-13 18:10:11 +0900

    Update app.py
    메일 기본 내용 문구 수정

--- Assess_Desk/Assess_Mail_Automation/app.py
+++ Assess_Desk/Assess_Mail_Automation/app.py
@@ -1,41 +1,53 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-import imaplib
-import time
-import os
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
-
-app = Flask(__name__)
-CORS(app) # 웹앱에서 접근 가능하게 허용
-
-def create_draft_logic(recipient, doc_type, sender_name):
-    """실제로 메일을 만드는 로직 (함수화)"""
+# app.py 내의 함수 수정
+def create_draft_logic(recipient, cc, subject):
     try:
         msg = MIMEMultipart('related')
         msg['To'] = recipient
-        msg['Subject'] = f"[{doc_type}] 요청하신 서류 안내드립니다."
+        msg['Cc'] = cc # 참조자 추가
+        msg['Subject'] = subject # 시트에서 받아온 제목 사용
         msg['From'] = "pys@vertexid.com"
 
-        # HTML 본문 (가로 309, 세로 177 고정)
+        # 요청하신 서식, 공백, 표를 HTML로 완벽히 구현했습니다.
         html_body = f"""
         <html>
-            <body style="font-family: 'Malgun Gothic', sans-serif;">
-                <p>안녕하세요, <b>{sender_name}</b>입니다.</p>
-                <p>요청하신 <b>{doc_type}</b> 관련하여 안내드립니다.</p>
-                <p>현재 초안이 작성되었습니다. 그룹웨어에서 확인 후 발송 부탁드립니다.</p>
+            <body style="font-family: 'Malgun Gothic', sans-serif; font-size: 10pt; color: black;">
                 <br>
-                <div style="margin-top: 30px; border-top: 1px solid #eee; padding-top: 20px;">
-                    <p>감사합니다.</p>
-                    <img src="cid:sig_img" width="309" height="177" style="display:block; width:309px; height:177px;">
-                </div>
+                <br>
+                안녕하세요 버텍스아이디 박영석입니다.<br>
+                <br>
+                {subject} 송부드립니다.<br>
+                <br>
+                <br>
+                <span style="color: blue; font-weight: bold;">소견서 및 견적서 요청 외 다른 문의</span>는 아래 연락처로 부탁드립니다.<br>
+                <br>
+                <table border="1" style="border-collapse: collapse; text-align: center; width: 350px; font-size: 10pt;">
+                    <tr>
+                        <th colspan="2" style="padding: 5px; font-weight: bold;">버텍스아이디 박용욱 실장</th>
+                    </tr>
+                    <tr>
+                        <td style="padding: 5px; width: 30%;">Phone</td>
+                        <td style="padding: 5px;">010-9914-7460</td>
+                    </tr>
+                    <tr>
+                        <td style="padding: 5px;">E-mail</td>
+                        <td style="padding: 5px;">pyw@vertexid.com</td>
+                    </tr>
+                </table>
+                <br>
+                <br>
+                감사합니다.<br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <img src="cid:sig_img" width="309" height="177" style="display:block; width:309px; height:177px; border:none;">
             </body>
         </html>
         """
         msg.attach(MIMEText(html_body, 'html', 'utf-8'))
 
-        # 이미지 처리 (상대 경로)
+        # --- 이미지 첨부 및 IMAP 접속 로직은 기존과 동일하게 유지 ---
+        # (아래는 기존 코드 그대로 두시면 됩니다)
         current_dir = os.path.dirname(os.path.abspath(__file__))
         img_path = os.path.join(current_dir, "mail_sign.png")
         with open(img_path, "rb") as f:
@@ -43,7 +55,6 @@ def create_draft_logic(recipient, doc_type, sender_name):
             img.add_header('Content-ID', '<sig_img>')
             msg.attach(img)
 
-        # IMAP 저장
         imap = imaplib.IMAP4("mail.vertexid.com", 143)
         imap.login("pys@vertexid.com", "vertex1q2w3e!") 
         imap.append("DRAFTS", '', imaplib.Time2Internaldate(time.time()), msg.as_bytes())
@@ -52,23 +63,19 @@ def create_draft_logic(recipient, doc_type, sender_name):
     except Exception as e:
         return False, str(e)
 
-# --- 외부에서 접속하는 통로 ---
+# API 창구도 변수를 받도록 수정
 @app.route('/make-draft', methods=['POST'])
 def make_draft_api():
-    data = request.get_json() # 웹앱에서 보낸 데이터를 읽음
+    data = request.get_json()
     
+    # 웹앱에서 보내준 3가지 핵심 데이터를 받습니다.
     success, message = create_draft_logic(
-        recipient=data.get('recipient'),
-        doc_type=data.get('doc_type'),
-        sender_name=data.get('sender_name', '관리자')
+        recipient=data.get('recipient', ''),
+        cc=data.get('cc', ''),
+        subject=data.get('subject', '소견서 안내')
     )
 
     if success:
         return jsonify({"status": "success", "message": "작성 완료"}), 200
     else:
         return jsonify({"status": "error", "message": message}), 500
-
-if __name__ == '__main__':
-    # 서버 실행
-    print("메일 작성 API 서버가 가동되었습니다!")
-    app.run(host='0.0.0.0', port=5000)
